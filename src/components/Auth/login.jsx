5abd3073fe78c86b28461bd0c57391e71268ffeb
Dinesh Singh <dkadesh1999@gmail.com>
2024-06-08 01:18:35 +0530

    this

---
+++ src/components/Auth/login.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const login = () => {
+  return (
+    <>
+      <div id="form-container">
+        <div className="container">
+          <div className="sub-container">
+            <h2>Login Form</h2>
+            <form action="">
+              <div id="">
+                <div className="form-group">
+                  <div>
+                    <label for="name">
+                      Email / Password <span className="star">*</span>
+                    </label>
+                  </div>
+                  <div>
+                    <input type="text" id="name" name="name" required />
+                  </div>
+                </div>
+              </div>
+              <div id="">
+                <div className="form-group">
+                  <div>
+                    <label for="conform_password">
+                      Enter your password <span className="star">*</span>
+                    </label>
+                  </div>
+                  <div>
+                    <input
+                      type="password"
+                      id="conform_password"
+                      name="conform_password"
+                      required
+                    />
+                  </div>
+                </div>
+              </div>
+              <div id="button">
+                <div className="button">
+                  <button type="submit">Login</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default login;
---
+++ src/components/Auth/signup.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+
+const signup = () => {
+  return (
+    <>
+      <div id="form-container">
+        <div className="container">
+          <div className="sub-container">
+            <h2>Signup Form</h2>
+            <form action="">
+              <div id="">
+                <div className="form-group">
+                  <div>
+                    <label for="name">Name <span className="star">*</span></label>
+                  </div>
+                  <div>
+                    <input type="text" id="name" name="name" required />
+                  </div>
+                </div>
+              </div>
+              <div id="">
+                <div className="form-group">
+                  <div>
+                    <label for="email">Name <span className="star">*</span></label>
+                  </div>
+                  <div>
+                    <input type="email" id="email" name="email" required />
+                  </div>
+                </div>
+              </div>
+              <div id="">
+                <div className="form-group">
+                  <div>
+                    <label for="phone">Phone <span className="star">*</span></label>
+                  </div>
+                  <div>
+                    <input type="tel" id="phone" name="phone" required />
+                  </div>
+                </div>
+              </div>
+              <div id="">
+                <div className="form-group">
+                  <div>
+                    <label for="set_password">Set password <span className="star">*</span></label>
+                  </div>
+                  <div>
+                    <input
+                      type="password"
+                      id="set_password"
+                      name="set_password"
+                      required
+                    />
+                  </div>
+                </div>
+              </div>
+              <div id="">
+                <div className="form-group">
+                  <div>
+                    <label for="conform_password">Set password <span className="star">*</span></label>
+                  </div>
+                  <div>
+                    <input
+                      type="password"
+                      id="conform_password"
+                      name="conform_password"
+                      required
+                    />
+                  </div>
+                </div>
+              </div>
+              <div id="button">
+                <div className="button">
+                <button type="submit">Sign Up</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default signup;
