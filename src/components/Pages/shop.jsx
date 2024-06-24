ddd1403b843b1bcf653cf7f4d7011d0c1941f724
Dinesh Singh <dkadesh1999@gmail.com>
2024-06-24 22:55:14 +0530

    this

--- src/components/Pages/shop.jsx
+++ src/components/Pages/shop.jsx
@@ -1,9 +1,27 @@
 const shop = () => {
-    return (
-        <>
-        shop
-        </>
-    );
-}
+  return (
+    <>
+      <div id="main">
+        <div className="main-section">
+          <div className="second-section">
+            <div className="first-section">
+              <div className="all">
+                <h2> All Products</h2>
+              </div>
+              <div className="filter">
+                <select name="Default sorting" id="">
+                  <option value="">Default sorting</option>
+                  <option value="">high to low</option>
+                  <option value="">low to high</option>
+                  <option value=""> Sort by top Brand </option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default shop;
