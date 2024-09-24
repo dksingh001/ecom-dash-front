271cea7a0b63f761340593d837d94147234c240a
Dinesh Singh <dkadesh1999@gmail.com>
2024-09-25 00:40:22 +0530

    this

--- src/components/Pages/Cart.jsx
+++ src/components/Pages/Cart.jsx
@@ -1,43 +1,82 @@
 import React, { useEffect, useState } from "react";
 import pic from "../../components/Assest/img/animation.png";
-import {useMain} from "../../hook/useMain"
+import { useMain } from "../../hook/useMain";
 
 const Cart = () => {
-  const {fetchallcartItem} = useMain();
-  const [cart, setCart] = useState([])
+  const { fetchallcartItem } = useMain();
+  const [cart, setCart] = useState([]);
 
-  useEffect (()=>{
-  const FetchcartItem = async() =>{
-    try {
-      const data = await fetchallcartItem();
-      if (data  && data.success) {
-        // setCart(data.cartitems)
-        // setCart((predata) =>[...predata, data.cartitems])
-        if (Array.isArray(data.cartitems)) {
-          setCart(data.cartitems)
-        
+  useEffect(() => {
+    const FetchcartItem = async () => {
+      try {
+        const data = await fetchallcartItem();
+        if (data && data.success) {
+          // setCart(data.cartitems)
+          // setCart((predata) =>[...predata, data.cartitems])
+          if (Array.isArray(data.cartitems)) {
+            setCart(data.cartitems);
+          } else {
+            console.error("Fetch data is not an array", data.cartitems);
+          }
         } else {
-          console.error("Fetch data is not an array", data.cartitems)
+          console.error("Failed to fetch cart Items", data?.message);
         }
-      }else{
-        console.error("Failed to fetch cart Items", data?.message)
+      } catch (error) {
+        console.error("Error fetching cart Items", error);
       }
-    } catch (error) {
-      console.error("Error fetching cart Items", error)
-    }
-
-  }
-  FetchcartItem()
-  }, [fetchallcartItem])
-  console.log(cart)
+    };
+    FetchcartItem();
+  }, [fetchallcartItem]);
+  // console.log(cart);
 
   return (
     <>
       <div id="cart-pages">
         <div className="cart-2nd">
           <div className="left-caintainer">
-            <div className="product-details">
-              <div class="cart-item">
+            {cart &&
+              cart.map((item) => (
+                <div className="product-details">
+                  <div class="cart-item">
+                    <div className="ctms">
+                      <div className="item-details-container">
+                        <div className="item-details-img">
+                          <img
+                            src={item.image}
+                            alt="LEBAS Women Kurta Pant Set"
+                          />
+                        </div>
+                        <div className="item-details-details">
+                          <div className="item-details-title">
+                            <h3 id="h3">{item.title}</h3>
+                          </div>
+                          <div className="item-details-size">
+                            <p id="ss">Size: </p>
+                            <p id="ssl">XL</p>
+                          </div>
+                          <div class="item-details-price">
+                            <p className="price">₹2,399</p>
+                            <p id="discount">₹720</p>
+                            <p className="percentage">60% off</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="item-details-dcrinrsa-re">
+                        <div className="decinr">
+                          <button className="nega">-</button>
+                          <button className="count">1</button>
+                          <button className="pule">+</button>
+                        </div>
+                        <div className="saflete">
+                          <button className="savelater">SAVE FOR LATER</button>
+                        </div>
+                        <div className="remo">
+                          <button className="remove">REMOVE</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  {/* <div class="cart-item">
                 <div className="item-details-container">
                   <div className="item-details-img">
                     <img src={pic} alt="LEBAS Women Kurta Pant Set" />
@@ -70,44 +109,11 @@ const Cart = () => {
                     <button className="remove">REMOVE</button>
                   </div>
                 </div>
-              </div>
-              <div class="cart-item">
-                <div className="item-details-container">
-                  <div className="item-details-img">
-                    <img src={pic} alt="LEBAS Women Kurta Pant Set" />
-                  </div>
-                  <div className="item-details-details">
-                  <div className="item-details-title">
-                    <h3 id="h3">LEBAS Women Kurta Pant Set</h3>
-                  </div>
-                  <div className="item-details-size">
-                    <p id="ss">Size: </p>
-                    <p id="ssl">XL</p>
-                  </div>
-                  <div class="item-details-price">
-                    <p className="price">₹2,399</p>
-                    <p id="discount">₹720</p>
-                    <p className="percentage">60% off</p>
-                  </div>
-                  </div>
+                    </div> */}
                 </div>
-                <div className="item-details-dcrinrsa-re">
-                  <div className="decinr">
-                    <button className="nega">-</button>
-                    <button className="count">1</button>
-                    <button className="pule">+</button>
-                  </div>
-                  <div className="saflete">
-                    <button className="savelater">SAVE FOR LATER</button>
-                  </div>
-                  <div className="remo">
-                    <button className="remove">REMOVE</button>
-                  </div>
-                </div>
-              </div>
-              <div className="cart-button">
-                <button type="button">PLACE ORDER</button>
-              </div>
+              ))}
+            <div className="cart-button">
+              <button type="button">PLACE ORDER</button>
             </div>
           </div>
           <div className="right-caintainer">
